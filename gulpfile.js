dc9ab619b901a3f29d158c544c356d1f5289b142
Xuan5422 <erenkov2010@ya.ru>
2021-12-26 20:00:52 +0300

    start

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,11 @@ gulp.task('server', function() {
         }
     });
     gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/css/*.*").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(gulp.dest("src/css"))
             .pipe(rename({suffix: '.min', prefix: ''}))
@@ -26,8 +27,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change",browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
